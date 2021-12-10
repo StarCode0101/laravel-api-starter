fd133da1dd0ebc6a085887db7fec90323313a19e
Bakare Abiola <murphybiola007@gmail.com>
2021-12-10 02:09:55 +0100

    updated

--- app/Http/Controllers/Auth/TwoFactorAuthController.php
+++ app/Http/Controllers/Auth/TwoFactorAuthController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
 use Illuminate\Http\Request;
-use Laravel\Fortify\Contracts\TwoFactorAuthenticationProvider;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TwoFactorAuthController extends Controller
 {
@@ -14,9 +13,15 @@ class TwoFactorAuthController extends Controller
         $confirmed = $request->user()->confirmTwoFactorAuth($request->code);
 
         if (!$confirmed) {
-            return back()->withErrors('Invalid Two Factor Authentication code');
+            return $request->wantsJson()
+            ? new JsonResponse('Invalid Two Factor Authentication code', 403)
+            : back()->with('status','Invalid Two Factor Authentication code');
+
         }
 
-        return back();
+        return $request->wantsJson()
+        ? new JsonResponse('Two factor authentication code has been setup succesfully', 200)
+        : back()->with('status', 'Two factor authentication code has been setup succesfully');
+        
     }
 }
